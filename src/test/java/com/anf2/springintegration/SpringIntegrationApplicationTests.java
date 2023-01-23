2d0c5174dcc9e28be6b84d1461f210f0f80034a0
stepan <stepan.anfinogenov@gmail.com>
2023-01-23 12:25:54 +0000

    init simple variant

---
+++ src/test/java/com/anf2/springintegration/SpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anf2.springintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
